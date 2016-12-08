be89ea8b0328b05512ad958b8202160711b19621
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-12-08 15:39:33 -0700

    add Adam's new things

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,3 +1,3 @@
-require('./main');
+require('./new');
 require('../CSS/reset.scss');
 require('../CSS/styles.scss');
--- lib/main.jsx
+++ lib/main.jsx
@@ -5,11 +5,11 @@ var Header = require('./components/Header');
 var Body = require('./components/Body');
 
 
-$.get('http://weatherly-api.herokuapp.com/api/weather').then(function(data) {
-  data.forEach( function(city) {
-    console.log(city.location, city.date);
-  });
-});
+// $.get('http://weatherly-api.herokuapp.com/api/weather').then(function(data) {
+//   data.forEach( function(city) {
+//     console.log(city.location, city.date);
+//   });
+// });
 
 class Weatherly extends React.Component {
   render() {
@@ -21,5 +21,3 @@ class Weatherly extends React.Component {
     );
   }
 }
-
-ReactDOM.render(<Weatherly />, document.getElementById('application'));
