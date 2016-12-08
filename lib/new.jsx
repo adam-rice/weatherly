737c57785dc94e6f88272c52d640d7d43c8bcdb1
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-12-08 15:56:23 -0700

    add weather to the page

--- lib/new.jsx
+++ lib/new.jsx
@@ -40,10 +40,29 @@ class Main extends React.Component {
             }}>
             Locate
           </button>
+          <WeatherCards weather={this.state.weather} />
         </section>
       </div>
     )
   }
 }
+
+const WeatherCards = (props) => {
+  let { weather } = (props);
+  if(!weather) {
+    return (
+      <div>Please Enter a Location</div>
+    );
+  }
+  return (
+    <div className='Weather-Card'>
+      {weather.map((card) => <div>
+        <li>{card.location}</li>
+        <li>{card.temp.high}</li>
+        <li>{card.temp.low}</li>
+      </div>)}
+    </div>
+  )
+};
 
 ReactDOM.render(<Main source='https://weatherly-api.herokuapp.com/api/weather/'/>, document.getElementById('application'));
